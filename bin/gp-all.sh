7fb8b604a0f4eb24ce7bf941b925b35da5d06225
Tom W Hartung <tomwhartung@gmail.com>
2015-07-28 22:11:20 -0600

    Run git pull in all currently active git repos.

---
+++ bin/gp-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Run git pull in all currently active git repos
+#
+cd /var/www/joomoowebsites.com/htdocs/subsites/idMyGadget	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/subsites/resume	; git pull 
+
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_beez3_idMyGadget	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_mod_menu_idMyGadget	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_protostar_idMyGadget	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_tomh_idMyGadget	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_idMyGadget_for_joomla	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_joomla_accoutrements	; git pull 
+

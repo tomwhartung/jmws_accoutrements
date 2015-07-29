7198ad1e84e6b1ab804e11ec004aa893590cf870
Tom W Hartung <tomwhartung@gmail.com>
2015-07-28 20:45:31 -0600

    Removed crufty linkModuleDirs and added reusable checkForDirectory functions.

--- bin/link_subsites.py
+++ bin/link_subsites.py
@@ -8,24 +8,14 @@ from os import chdir, getcwd, listdir
 from os.path import isfile, isdir, islink
 from subprocess import call
 
-
-def linkModuleDirs( extension ) :
-	print( 'Linking modules directories for extension ' + extension + '...' )
-	rootedSourceDir = htdocsDir + '/' + subsitesDir + '/' + extension + '/modules'
-	destinationDir = htdocsDir + '/' + mainSiteDir + '/modules'
-	moduleDirs = listdir( rootedSourceDir )
-	### print( 'linkModuleDirs test: rootedSourceDir = ' + rootedSourceDir )
-	### print( 'linkModuleDirs test: destinationDir = ' + destinationDir )
-	### print( 'moduleDirs:' )
-	### print( moduleDirs )
-	for modDir in moduleDirs:
-		rootedModuleDir = rootedSourceDir + '/' + modDir
-		if( isdir(rootedModuleDir) ) :
-			print( '\tlinking "' + rootedModuleDir + "\" to\n\t\t\"" + destinationDir + '"' )
-			lnCommand = 'cd ' + destinationDir + '; ln -fs ' + rootedModuleDir + ' .; cd ' + htdocsDir 
-		### print( 'lnCommand: ' + lnCommand )
-			call( lnCommand, shell=True )
-
+def checkForDirectory( dirToCheck ) :
+	if isdir( dirToCheck ) :
+		print( 'Found a ' + dirToCheck + ' directory, cool.' )
+	else:
+		print( 'Expecting a "' + dirToCheck + '" directory, but it is not here.' )
+		print( 'The current directory is: "' + htdocsDir + '"' )
+		print( 'Change to a different directory, preferably one named "htdocs," and try again.' )
+		exit( 1 )
 
 def linkSubsite( subsite ) :
 	print( 'Linking the ' + subsite + ' subsite to ' + mainSiteDir + '...' )
@@ -39,21 +29,9 @@ mainSiteDir = 'joomoowebsites.com'
 htdocsDir = getcwd()
 exitVal = 0
 
-if isdir( subsitesDir ) :
-	print( 'Found a ' + subsitesDir + ' directory, cool.' )
-else:
-	print( 'Expecting a "' + subsitesDir + '" directory, but it is not here.' )
-	print( 'The current directory is: "' + htdocsDir + '"' )
-	print( 'Change to a different directory, preferably one named "htdocs," and try again.' )
-	exit( 1 )
+checkForDirectory( subsitesDir )
+checkForDirectory( mainSiteDir )
 
-if isdir( mainSiteDir ) :
-	print( 'Found a ' + mainSiteDir + ' directory, cool.' )
-else:
-	print( 'Expecting a "' + mainSiteDir + '" directory, but it is not here.' )
-	print( 'The current directory is: "' + htdocsDir + '"' )
-	print( 'Change to a different directory, preferably one named "htdocs," and try again.' )
-	exit( 1 )
 
 subsites = listdir( subsitesDir )
 subsites.sort()

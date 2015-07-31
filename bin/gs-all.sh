2abd734e1b30b0c74fc65e4ffe0b349f21ae68fc
Tom W Hartung <tomwhartung@gmail.com>
2015-07-31 10:18:01 -0600

    Run git status in all currently active git repos.

---
+++ bin/gs-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Run git status in all currently active git repos
+#
+cd /var/www/joomoowebsites.com/htdocs/subsites/idMyGadget; printf "\n========\n$(pwd):\n"; git status 
+cd /var/www/joomoowebsites.com/htdocs/subsites/resume; printf "\n========\n$(pwd):\n"; git status 
+
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_beez3_idMyGadget/ ; printf "\n========\n$(pwd):\n"; git status 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_idMyGadget_for_joomla/ ; printf "\n========\n$(pwd):\n"; git status 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_joomla_accoutrements/ ; printf "\n========\n$(pwd):\n"; git status 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_mod_menu_idMyGadget/ ; printf "\n========\n$(pwd):\n"; git status 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_protostar_idMyGadget/ ; printf "\n========\n$(pwd):\n"; git status 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_tomh_idMyGadget/ ; printf "\n========\n$(pwd):\n"; git status 
+

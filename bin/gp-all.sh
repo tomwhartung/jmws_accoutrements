ac7200307aae33a3506554554eb64896a41dc948
Tom W Hartung <tomwhartung@gmail.com>
2015-08-03 12:52:45 -0600

    Updated to process idMyGadget and resume repositories in their new "main" place of residence, /var/www/subsites .  Also made use of whitespace a bit more consistent.

--- bin/gp-all.sh
+++ bin/gp-all.sh
@@ -2,13 +2,13 @@
 #
 # Run git pull in all currently active git repos
 #
-cd /var/www/joomoowebsites.com/htdocs/subsites/idMyGadget	; git pull 
-cd /var/www/joomoowebsites.com/htdocs/subsites/resume	; git pull 
+cd /var/www/subsites/idMyGadget ;	git pull
+cd /var/www/subsites/resume ;	git pull
 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_beez3_idMyGadget	; git pull 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_mod_menu_idMyGadget	; git pull 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_protostar_idMyGadget	; git pull 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_tomh_idMyGadget	; git pull 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_idMyGadget_for_cmses	; git pull 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_accoutrements	; git pull 
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_accoutrements ;	git pull
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_beez3_idMyGadget ;	git pull
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_idMyGadget_for_cmses ;	git pull
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_mod_menu_idMyGadget ;	git pull
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_protostar_idMyGadget ;	git pull
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_tomh_idMyGadget ;	git pull
 

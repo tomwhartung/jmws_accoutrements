e0b94425834ff39a76966a47ea4b58cfa5e7c61e
Tom W Hartung <tomwhartung@gmail.com>
2015-08-06 13:50:25 -0600

    Prettified output.

--- bin/gs-all.sh
+++ bin/gs-all.sh
@@ -2,17 +2,18 @@
 #
 # Run git status in all currently active git repos
 #
-cd /var/www/jmws_accoutrements ;	printf "\n========\n$(pwd):\n" ;	git status
+dash_equals='-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-'
+cd /var/www/jmws_accoutrements ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
 
-cd /var/www/subsites/idMyGadget ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/subsites/resume ;	printf "\n========\n$(pwd):\n" ;	git status
+cd /var/www/subsites/idMyGadget ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+cd /var/www/subsites/resume ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
 
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_accoutrements/ ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_beez3_idMyGadget/ ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_idMyGadget_for_joomla/ ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_mod_menu_idMyGadget/ ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_protostar_idMyGadget/ ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_tomh_idMyGadget/ ;	printf "\n========\n$(pwd):\n" ;	git status
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_beez3_idMyGadget/ ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_idMyGadget_for_joomla/ ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_mod_menu_idMyGadget/ ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_protostar_idMyGadget/ ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+cd /var/www/joomoowebsites.com/htdocs/customizations/jmws_tomh_idMyGadget/ ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
 
-cd /var/www/tomwhartung.com/htdocs/customizations/jmws_idMyGadget_for_wordpress ;	printf "\n========\n$(pwd):\n" ;	git status
-cd /var/www/tomwhartung.com/htdocs/customizations/jmws_twentyfifteen_idMyGadget ;	printf "\n========\n$(pwd):\n" ;	git status
+cd /var/www/tomwhartung.com/htdocs/customizations/jmws_idMyGadget_for_wordpress ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+cd /var/www/tomwhartung.com/htdocs/customizations/jmws_twentyfifteen_idMyGadget ;	printf "\n$dash_equals\n$(pwd):\n" ;	git status
+

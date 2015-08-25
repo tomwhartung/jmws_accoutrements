7a2c915c6934af2611967d7fefbbe061405c9eb3
Tom W Hartung <tomwhartung@gmail.com>
2015-08-25 12:58:55 -0600

    Making good progress on our new ln_drupal_customizations.py program.

--- bin/ln_drupal_customizations.py
+++ bin/ln_drupal_customizations.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------------------
 #
 import os
-from os import chdir, getcwd, listdir
+from os import chdir, getcwd, listdir, makedirs
 from os.path import isfile, isdir, islink
 from subprocess import call
 
@@ -21,37 +21,37 @@ def checkForDirectory( dirToCheck ) :
 		exit( 1 )
 
 ##
-#  Links directories containing plugins
+#  Links directories containing jmws modules
 #
-def lnPluginsDirs( customization ) :
-	print( 'Linking wp-content/plugins directories for customization ' + customization + '...' )
-	rootedSourceDir = htdocsDir + '/' + customizationsDir + '/' + customization + '/wp-content/plugins'
-	destinationDir = htdocsDir + '/' + mainSiteDir + '/wp-content/plugins'
-	wpContentPlugins = listdir( rootedSourceDir )
-##	print( 'lnPluginsDirs: rootedSourceDir = ' + rootedSourceDir )
-##	print( 'lnPluginsDirs: destinationDir = ' + destinationDir )
-##	print( 'lnPluginsDirs: wpContentPlugins = ', end="" )
-##	print( wpContentPlugins )
-	for themeDir in wpContentPlugins :
-		rootedThemeDir = rootedSourceDir + '/' + themeDir
-		if( isdir(rootedThemeDir) ) :
-			print( '\tlinking "' + rootedThemeDir + "\" to\n\t\t\"" + destinationDir + '"' )
-			lnCommand = "    cd " + destinationDir + ";\n    ln -fs " + rootedThemeDir + " .;\n    cd " + htdocsDir 
+def lnModulesDirs( customization ) :
+	print( 'Linking sites/all/modules/jmws directories for customization ' + customization + '...' )
+	rootedSourceDir = htdocsDir + '/' + customizationsDir + '/' + customization + '/sites/all/modules/jmws'
+	destinationDir = htdocsDir + '/' + mainSiteDir + '/sites/all/modules/jmws'
+	drupalJmwsModules = listdir( rootedSourceDir )
+	print( 'lnModulesDirs: rootedSourceDir = ' + rootedSourceDir )
+	print( 'lnModulesDirs: destinationDir = ' + destinationDir )
+	print( 'lnModulesDirs: drupalJmwsModules = ', end="" )
+	print( drupalJmwsModules )
+	for moduleDir in drupalJmwsModules :
+		rootedModuleDir = rootedSourceDir + '/' + moduleDir
+		if( isdir(rootedModuleDir) ) :
+			print( '\tlinking "' + rootedModuleDir + "\" to\n\t\t\"" + destinationDir + '"' )
+			lnCommand = "    cd " + destinationDir + ";\n    ln -fs " + rootedModuleDir + " .;\n    cd " + htdocsDir 
 			print( "lnCommand:\n" + lnCommand )
 			call( lnCommand, shell=True )
 
 ##
-#  Links directories containing themes
+#  Links directories containing jmws themes
 #
 def lnThemesDirs( customization ) :
-	print( 'Linking wp-content/themes directories for customization ' + customization + '...' )
-	rootedSourceDir = htdocsDir + '/' + customizationsDir + '/' + customization + '/wp-content/themes'
-	destinationDir = htdocsDir + '/' + mainSiteDir + '/wp-content/themes'
+	print( 'Linking sites/all/themes/jmws directories for customization ' + customization + '...' )
+	rootedSourceDir = htdocsDir + '/' + customizationsDir + '/' + customization + '/sites/all/themes/jmws'
+	destinationDir = htdocsDir + '/' + mainSiteDir + '/sites/all/themes/jmws'
 	wpContentThemes = listdir( rootedSourceDir )
-##	print( 'lnThemesDirs: rootedSourceDir = ' + rootedSourceDir )
-##	print( 'lnThemesDirs: destinationDir = ' + destinationDir )
-##	print( 'lnThemesDirs: wpContentThemes = ', end="" )
-##	print( wpContentThemes )
+	print( 'lnThemesDirs: rootedSourceDir = ' + rootedSourceDir )
+	print( 'lnThemesDirs: destinationDir = ' + destinationDir )
+	print( 'lnThemesDirs: wpContentThemes = ', end="" )
+	print( wpContentThemes )
 	for themeDir in wpContentThemes :
 		rootedThemeDir = rootedSourceDir + '/' + themeDir
 		if( isdir(rootedThemeDir) ) :
@@ -64,28 +64,50 @@ def lnThemesDirs( customization ) :
 #  Driver function to call other functions to link specific types of customizations
 #
 def lnCustomization( customization ) :
-	print( 'Linking files in the ' + customization + ' customization...' )
-	wpContentDir = customizationsDir + '/'  + customization + '/' + 'wp-content'
-	wpContentSubdirs = listdir( wpContentDir )
-	wpContentSubdirs.sort()
-	for subdirectory in wpContentSubdirs:
-		if ( subdirectory == 'plugins' ) :
-			lnPluginsDirs( customization )
+	print( 'Linking directories in the ' + customization + ' customization...' )
+	customizationSitesAllDir = customizationsDir + '/'  + customization + '/sites/all'
+	sitesAllModulesJmwsSubdirs = listdir( customizationSitesAllDir )
+	sitesAllModulesJmwsSubdirs.sort()
+	for subdirectory in sitesAllModulesJmwsSubdirs:
+		if ( subdirectory == 'modules' ) :
+			lnModulesDirs( customization )
 		elif ( subdirectory == 'themes' ) :
 			lnThemesDirs( customization )
 
 customizationsDir = 'customizations'
-mainSiteDir = 'tomwhartung.com'
+mainSiteDir = 'tomhartung.com'
 htdocsDir = getcwd()
 exitVal = 0
 
 checkForDirectory( customizationsDir )   # exits if directory not present
 checkForDirectory( mainSiteDir )         # exits if directory not present
 
+mainSitesAllDir = mainSiteDir + '/sites/all'
+mainSitesAllModulesJmwsDir = mainSitesAllDir + '/modules/jmws'
+mainSitesAllThemesJmwsDir = mainSitesAllDir + '/themes/jmws'
+
+if isdir( mainSitesAllDir ) :
+	if isdir( mainSitesAllModulesJmwsDir ) :
+		print( 'Found a ' + mainSitesAllModulesJmwsDir + ' directory, cool!' )
+	else :
+		print( 'Warning: making the ' + mainSitesAllModulesJmwsDir + ' directory, because it was not found.' )
+		os.makedirs( mainSitesAllModulesJmwsDir )
+else :
+	print( 'Error!' )
+	print( 'Not seeing a ' + mainSitesAllDir + ' directory!' )
+	print( 'This does not look like drupal code!' )
+	print( 'Exiting!' )
+	exit( 1 )
+
 customizations = listdir( customizationsDir )
 customizations.sort()
 print( 'Linking the following customizations to the appropriate directories in ' + mainSiteDir + ':' )
 print( customizations )
+
+print( 'So far so good!' )
+
+exit( exitVal )
+
 
 for customization in customizations:
 	lnCustomization( customization )

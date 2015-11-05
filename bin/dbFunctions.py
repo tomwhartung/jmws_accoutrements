1df9773b14776af73c8367055a432c54577be834
Tom W Hartung <tomwhartung@gmail.com>
2015-11-04 19:44:01 -0700

    Added function getDbBackupFileBaseName() .

--- bin/dbFunctions.py
+++ bin/dbFunctions.py
@@ -5,7 +5,9 @@
 # Any code that both dbMySqlBackup and dbMySqlRestore can use belongs in a function in this file
 #
 import os       # for getting values for environment vars
+import socket   # for getting the hostName
 import sys      # for accessing command line arguments
+import time     # for the date string in our backup file name
 
 def processArguments( backupOrRestore ) :
 	siteArg = ''
@@ -35,8 +37,8 @@ def syntax( backupOrRestore ) :
 	print( '  site.com-YYYY_MM_DD-hostname[-suffix].sql.gz' )
 
 #
-# Check the environment for a setting for the backup directory
-# If it is not set, use the default
+#  Check the environment for a setting for the backup directory
+#  If it is not set, use the default
 #
 def getDbBackupDirectory() :
 	dbBackupDirectoryDefault = '.'
@@ -50,4 +52,19 @@ def getDbBackupDirectory() :
 	else :
 		dbBackupDirectory = dbBackupDirectoryEnviron
 	return dbBackupDirectory
-	
+
+#
+#  Assemble the basename of the backup file, using a variety of identifying components
+#
+def getDbBackupFileBaseName( siteName, suffixArg ) :
+	dateString = time.strftime( '-%Y_%m_%d-' )
+	hostName = socket.gethostname()
+	suffixString = ''
+	#
+	# If we have a suffix, prefix it with a dash, to separate it from the hostname
+	#
+	if( suffixArg != '' ) :
+		suffixString = '-' + suffixArg
+	dbBackupFileName = siteName + dateString + hostName + suffixString + '.sql.tgz'
+	return dbBackupFileName
+

d631fd0bf9dc435e560d0835ce21d26d255c7174
Tom W Hartung <tomwhartung@gmail.com>
2015-11-07 15:10:00 -0700

    We can now run git status in the core directory of each site in the list.

--- bin/gitSitesListExample.py
+++ bin/gitSitesListExample.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #
-# gitSites.py: define an array of site names that have code in github
-# -------------------------------------------------------------------
+# gitSitesListExample.py: define an array of site names that have code in github
+# ------------------------------------------------------------------------------
 #
-gitSites = [ 'joomoowebsites.com', 'tomhartung.com', 'tomwhartung.com']
+gitSiteNames = ['joomoowebsites.com', 'tomhartung.com', 'tomwhartung.com']
 

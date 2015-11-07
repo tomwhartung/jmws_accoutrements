9dae2fa6b3ca0b1e20a030d25dc1ec9fdb12ecd5
Tom W Hartung <tomwhartung@gmail.com>
2015-11-07 12:59:44 -0700

    Adding gitSitesList.py to define an array of site names that have code in github .

---
+++ bin/gitSitesList.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#
+# gitSites.py: define an array of site names that have code in github
+# -------------------------------------------------------------------
+#
+gitSites = [ 'joomoowebsites.com', 'tomhartung.com', 'tomwhartung.com']
+

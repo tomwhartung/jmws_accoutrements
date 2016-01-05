702522100983a5968c20ec51663cd686396bdf6f
Tom W Hartung <tomwhartung@gmail.com>
2016-01-04 18:48:07 -0700

    Updated to print a help message if the user specifies "-h", "-help", or "--help".

--- bin/purgeOldKernels.py
+++ bin/purgeOldKernels.py
@@ -23,21 +23,30 @@ def syntax() :
 #
 def getKernelVersion () :
 	kernelVersion = ''
+	helpMessage = 'Run "dpkg -l | grep linux-image" and supply the numeric part (e.g., 3.13.0-55).'
 	if ( len(sys.argv) == 1 ) :
 		syntax()
 		print( 'No kernel version specified!' )
-		print( 'Run "dpkg -l | grep linux-image" and supply the numeric part (e.g., 3.13.0-55).' )
+		print( helpMessage )
 		print( 'Exiting.' )
 		exit( 1 )
 	elif ( len(sys.argv) == 2 ) :
+		if ( sys.argv[1] == '-h' or sys.argv[1] == '-help' or sys.argv[1] == '--help' ) :
+			syntax()
+			print( helpMessage )
+			exit( 0 )
 		kernelVersion = sys.argv[1]
 	else :
 		print( 'Too many arguments, try again.' )
 		exit( 1 )
 	return kernelVersion
 
+##
+# Verify user is not trying to delete the kernel currently being used
+#
 
 kernelVersion = getKernelVersion()
+
 print( 'Deleting kernel version ' + kernelVersion )
 
 aptGetCommand  = 'apt-get purge '
